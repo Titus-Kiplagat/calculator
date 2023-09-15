d581cc3aed933ec8a49b9386e8088c0bfe7a349f
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-15 20:19:15 +0300

    Modify the fetch url to display quote on UI

--- src/components/QuoteDisplay.jsx
+++ src/components/QuoteDisplay.jsx
@@ -1,45 +1,44 @@
 import { useEffect, useState } from 'react';
 
 function QuoteDisplay() {
-  const [quote, setQuote] = useState('');
-  const [author, setAuthor] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, setState] = useState({
+    quote: 'loading...',
+    author: '',
+    loading: true,
+    error: null,
+  });
+
+  const {
+    quote, author, loading, error,
+  } = state;
 
   useEffect(() => {
-    const timeout = setTimeout(() => {
-      const fetchQuote = async () => {
-        const quoteUrl = process.env.REACT_APP_QUOTE_URL;
-        const apiKey = process.env.REACT_APP_API_KEY;
-        const category = 'learning';
+    const fetchQuote = async () => {
+      const quoteUrl = 'https://api.api-ninjas.com/v1/quotes';
+      const apiKey = '0vvAreMbCkJcbEpLLZXr0g==4T5cWEwXr0ei7waX';
+      const category = 'learning';
 
-        try {
-          const response = await fetch(`${quoteUrl}?category=${category}`, { headers: { 'X-Api-Key': `${apiKey}` } });
-          const data = await response.json();
+      try {
+        const response = await fetch(`${quoteUrl}?category=${category}`, { headers: { 'X-Api-Key': `${apiKey}` } });
+        const data = await response.json();
 
-          if (!response.ok) {
-            throw new Error('Failed to fetch quote.');
-          }
-
-          const { quote, author } = data[0];
-          setQuote(quote);
-          setAuthor(author);
-        } catch (error) {
-          setError(error.message);
-        } finally {
-          setLoading(false);
+        if (!response.ok) {
+          throw new Error('Failed to fetch quote.');
         }
-      };
 
-      fetchQuote();
-    }, 1000);
+        const { quote, author } = data[0];
+        setState((prevState) => ({ ...prevState, quote, author }));
+      } catch (error) {
+        setState((prevState) => ({ ...prevState, error: error.message }));
+      } finally {
+        setState((prevState) => ({ ...prevState, loading: false }));
+      }
+    };
+
+    const timeout = setTimeout(fetchQuote, 1000);
 
     return () => clearTimeout(timeout);
   }, []);
-
-  if (loading) {
-    return <div className="mt-10">Loading...</div>;
-  }
 
   if (error) {
     return (
@@ -52,21 +51,29 @@ function QuoteDisplay() {
   }
 
   return (
-    <div className="bg-gray-100 w-2/4 p-5 m-5 text-center rounded shadow-md shadow-gray-500 text-orange-900 text-lg font-black">
-      <h3>
-        &#34;
-        {quote}
-        &#34;
-        {' '}
-        by
-        {' '}
-        <span>
-          &#34;
-          {author}
-          &#34;
-        </span>
-      </h3>
-    </div>
+    <>
+      {
+        loading ? (
+          <div className="mt-10">Loading...</div>
+        ) : (
+          <div className="bg-gray-100 p-5 m-5 text-center rounded shadow-md shadow-gray-500 text-orange-900 text-lg font-black w-screen md:w-2/4">
+            <h3>
+              &#34;
+              {quote}
+              &#34;
+              {' '}
+              by
+              {' '}
+              <span>
+                &#34;
+                {author}
+                &#34;
+              </span>
+            </h3>
+          </div>
+        )
+}
+    </>
   );
 }
 
