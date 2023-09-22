f583e9828ceb6607a034c19963e2da398fb2fdf3
Titus Kiplagat <tituskiplagat272@gmail.com>
2023-09-22 03:03:54 +0300

    Modify contents of home page:

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,11 @@
-const Home = () => {
-  const pageContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nobis eveniet voluptates explicabo, impedit eos ducimus vel aliquam amet officiis molestias iure sit porro. Nemo incidunt sint delectus accusantium aspernatur! At ea dolores tempora autem numquam veniam cum iure neque soluta facere porro unde, odio, inventore temporibus recusandae distinctio! Sapiente atque qui voluptas accusantium provident reprehenderit.Repudiandae quam dicta reiciendis ?';
-
-  return (
-    <div className="m-4">
-      <p className="text-xl font-black my-10">Welcome to how page!</p>
-      <div className="text-thin">
-        <p className="my-4">{pageContent}</p>
-        <p className="my-4">{pageContent}</p>
-      </div>
+const Home = () => (
+  <div className="m-4 text-orange-900">
+    <p className="text-xl font-black my-10">Welcome to our Calculator App!</p>
+    <div className="text-thin">
+      <p className="my-4">Welcome to our calculator app! Explore the world of calculations and computations right at your fingertips. This calculator is perfect for everyday calculations, whether it&apos;s splitting bills with friends or solving homework problems. It&apos;s fast, reliable, and easy to use.</p>
+      <p className="my-4">Join us and unleash the potential of our advanced calculator app. Effortlessly solve mathematical problems and equations with precision and speed. Our app provides a wide range of functions and features that cater to various mathematical requirements. Make your calculations seamless and efficient with our intuitive calculator app. Get started now!</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Home;
