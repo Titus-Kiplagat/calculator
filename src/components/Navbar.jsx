dd6bd148d81e7876b70fd83b8bf17a47c82dc3ea
Titus Kiplagat <tituskiplagat272@gmail.com>
2024-01-18 15:49:28 +0300

    Add styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom';
 
 const Navbar = () => (
-  <nav className="sticky top-0 select-none bg-gray-500 lg:flex lg:items-stretch w-full">
-    <div className="flex flex-no-shrink items-stretch h-12">
+  <nav className="sticky top-0 select-none bg-gray-500 lg:flex lg:items-stretch w-full md:flex-row">
+    <div className="flex justify-center flex-no-shrink items-stretch h-12">
       <Link to="/" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark text-2xl font-black uppercase">Math Calculator</Link>
     </div>
     <div className="lg:flex lg:items-stretch lg:flex-no-shrink lg:flex-grow">
-      <div className="lg:flex lg:items-stretch lg:justify-end ml-auto">
+      <div className="flex justify-around lg:items-stretch  ml-auto">
         <Link to="home" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Home</Link>
         <Link to="calculator" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Calculator</Link>
         <Link to="quote" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center hover:bg-grey-dark">Quote</Link>
--- src/pages/CalculatorApp.jsx
+++ src/pages/CalculatorApp.jsx
@@ -1,7 +1,7 @@
 import Calculator from '../components/Calculator';
 
 const CalculatorApp = () => (
-  <div className="flex flex-col md:flex-row md:gap-48 m-4 md:my-10">
+  <div className="flex flex-col md:flex-row md:justify-around md:gap-48 m-4 md:my-10">
     <p className="text-xl font-black text-orange-900">Let&apos;s do some math!</p>
     <Calculator />
   </div>
